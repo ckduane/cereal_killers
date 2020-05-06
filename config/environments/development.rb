4508ec5de209e827f65ea2b670643c24654e0997
ckduane <ckduane@gmail.com>
2020-05-06 16:59:19 -0700

    add active storage gem, update env configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.active_storage.service = :google_dev
+
+# Store files locally.
+  config.active_storage.service = :local
+  #config.active_storage.service = :google_dev
 end
