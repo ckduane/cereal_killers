22e428326df63531026da00511fe6f90ac9e4930
ckduane <ckduane@gmail.com>
2020-05-11 15:27:28 -0700

    remove duplicate user controller, remove redundant users page

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -3,10 +3,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    
+
     respond_to do |format|
         format.html # show.html.erb
         format.xml { render :xml => @user }
     end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:avatar)
   end
 end
