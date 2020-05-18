f331c779bba6f6967cad72dd7f1903f02ee7405a
ckduane <ckduane@gmail.com>
2020-05-18 16:50:52 -0700

    fixed signout by adding back js

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_232537) do
+ActiveRecord::Schema.define(version: 2020_05_06_235423) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -39,11 +42,27 @@ ActiveRecord::Schema.define(version: 2020_05_11_232537) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-# Could not dump table "reviews" because of following StandardError
-#   Unknown type 'file' for column 'uploads'
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id", null: false
+    t.integer "rating", null: false
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'file' for column 'avatar'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
