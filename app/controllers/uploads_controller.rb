de806f1040b2b1d0e2ee966e28f001e11b81ad37
ckduane <ckduane@gmail.com>
2020-06-16 13:41:38 -0700

    update gray variable colors to be in line with red, add uploads controller, add ability to upload photo on product show page

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,24 @@
+class UploadsController < ApplicationController
+
+	def new
+		@product = find_product
+	end
+
+	def create
+		@product = Product.find_or_initialize_by(name: params[:product][:name])
+		@product.uploads.attach(params[:product][:uploads])
+		if @product.save
+			redirect_to product_path(@product)
+		else
+			@errors = @product.errors.full_messages
+			render :new
+		end
+	end
+
+	private
+
+	def find_product
+		@product = Product.find_by(id: params[:product_id])
+	end
+
+end
