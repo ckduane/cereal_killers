5003de1735f67167217e53eaf8cbe9eb39f5f1e7
ckduane <ckduane@gmail.com>
2020-06-18 16:50:29 -0700

    add file upload size warning

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -8,4 +8,13 @@ $(document).ready(function(){
 	  centerMode: true,
 	  variableWidth: true
   });
+
+  $('#fileInput').on('change',function(){
+	  if($(this).get(0).files.length > 0){
+	    var fileSize = $(this).get(0).files[0].size;
+	    if (fileSize > 1000000){
+	    	$('#fileInput').before("<div class='alert alert-danger'>Warning: Image is too large. Please resize to less than 1MB.</div>")
+	    }
+	  }
+	});
 });
