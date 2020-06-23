bf3e26c6361fc03cef02ba86dd33757e05271a8c
ckduane <ckduane@gmail.com>
2020-06-23 13:00:16 -0700

    fix data, update index tests

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get reviews_index_url
-    assert_response :success
-  end
 
 end
