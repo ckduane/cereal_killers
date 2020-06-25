84836a27f7cd2c037e889d1bc3dcfd1ea32ec026
ckduane <ckduane@gmail.com>
2020-06-25 14:47:42 -0700

    remove else if statement allowing user to upload product w/o review, remove from test scenario

--- test/integration/product_flow_test.rb
+++ test/integration/product_flow_test.rb
@@ -6,16 +6,4 @@ class ProductFlowTest < ActionDispatch::IntegrationTest
     assert_select "h1", "Your next cereal awaits"
   end
 
-  test "can create product" do
-		get "/products/new"
-		assert_response :success
-
-		post "/products",
-			params: { product: { name: 'Lucky Charms' } }
-		assert_response :redirect
-		follow_redirect!
-		assert_response :success
-		assert_select "h1", "Lucky Charms"
-	end
-
 end
