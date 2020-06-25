f133d24b953ee70c91482cc9f1331d41a807a98e
ckduane <ckduane@gmail.com>
2020-06-25 14:37:37 -0700

    add new product test

--- test/integration/product_flow_test.rb
+++ test/integration/product_flow_test.rb
@@ -5,4 +5,17 @@ class ProductFlowTest < ActionDispatch::IntegrationTest
     get "/"
     assert_select "h1", "Your next cereal awaits"
   end
+
+  test "can create product" do
+		get "/products/new"
+		assert_response :success
+
+		post "/products",
+			params: { product: { name: 'Lucky Charms' } }
+		assert_response :redirect
+		follow_redirect!
+		assert_response :success
+		assert_select "h1", "Lucky Charms"
+	end
+
 end
