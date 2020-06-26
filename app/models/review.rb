2afd175a6733ac6d8963e309f29e7f4f3ab1ae53
ckduane <ckduane@gmail.com>
2020-06-26 12:20:10 -0700

    fix model name breaking editing rich text

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,5 +6,5 @@ class Review < ApplicationRecord
 	validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 	validates_associated :product, :user
 
-	has_rich_text :content
+	has_rich_text :comment
 end
