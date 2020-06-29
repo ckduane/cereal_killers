39ce9cd9d5f6a19404f3a07fbbf4a44acf096c95
ckduane <ckduane@gmail.com>
2020-06-29 16:30:18 -0700

    add font awesome icons for next/prev slick arrows, add hover effect

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
   $('.slider').slick({
     dots: false,
-    arrows: false,
 	  infinite: true,
 	  speed: 300,
 	  slidesToShow: 1,
 	  centerMode: true,
-	  variableWidth: true
+	  centerPadding: '40px',
+	  variableWidth: true,
+	  nextArrow: '<i class="fas fa-angle-right"></i>',
+	  prevArrow: '<i class="fas fa-angle-left"></i>'
   });
 
   $('#fileInput').on('change',function(){
